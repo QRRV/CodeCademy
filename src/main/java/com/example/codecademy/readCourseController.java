66ec4fdd990c0bf9894e5193eeb83fe7526de7aa
Matthijs07g <matthijs07g@gmail.com>
2022-12-15 21:26:49 +0100

    scene switch code

--- src/main/java/com/example/codecademy/readCourseController.java
+++ src/main/java/com/example/codecademy/readCourseController.java
@@ -2,28 +2,24 @@ package com.example.codecademy;
 
 import java.io.IOException;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class HelloController {
+public class readCourseController {
+    
     @FXML
-    private Label welcomeText;
     private Stage stage;
+    private Label test;
 
     @FXML
-    protected void onHelloButtonClick(ActionEvent event) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("createCourse-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setScene(scene);
-        stage.show();
+    protected void test() throws IOException{
+        test.setText("test 1234");
     }
-} 
+}
+
 
 // public void start(Stage stage) throws IOException {
 //     FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
