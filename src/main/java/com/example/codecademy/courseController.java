4ad0fa9cd198fe53dcd65a8d665157452f394551
Matthijs <matthijs07g@gmail.com>
2022-12-16 13:21:08 +0100

    begin delete course

--- src/main/java/com/example/codecademy/courseController.java
+++ src/main/java/com/example/codecademy/courseController.java
@@ -8,6 +8,8 @@ import java.util.ResourceBundle;
 import com.example.codecademy.Domain.Course;
 import com.example.codecademy.Repository.CourseRepository;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -22,17 +24,23 @@ public class courseController extends CourseRepository{
     
     @FXML
     private ListView<Course> myListView;
-    private Label test1;
-
-    @FXML 
-    protected void test(){
-        test1.setText("test");
-    }
+    
+    String item;
 
     @FXML
     protected void loadCourse() throws SQLException{
         ArrayList<Course> courses = CourseRepository.getCourses();
         myListView.getItems().addAll(courses);
+    }
+
+    @FXML
+    protected void deleteCourse() throws SQLException{
+        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2){
+                item = myListView.getSelectionModel().getSelectedItem();
+            }
+        }); 
     }
 
     // @Override
