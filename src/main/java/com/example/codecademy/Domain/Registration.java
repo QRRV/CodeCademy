40fa7db6d3a1b8b75005af74a7f0c3a3aa584500
JDX-001 <jd.angeladelacruz@student.avans.nl>
2022-12-16 12:43:40 +0100

    Added create and update controller and view

---
+++ src/main/java/com/example/codecademy/Domain/Registration.java
@@ -0,0 +1,43 @@
+package com.example.codecademy.Domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+public class Registration {
+    private Date registrationate;
+    private ArrayList<Student> emailAddress = new ArrayList<>();
+    private Course course;
+    private Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public ArrayList<Student> getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(ArrayList<Student> emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Date getRegistrationate() {
+        return registrationate;
+    }
+
+    public void setRegistrationate(Date registrationate) {
+        this.registrationate = registrationate;
+    }
+
+}
